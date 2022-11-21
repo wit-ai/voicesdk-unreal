939db2dd26f7dd0c9e42b5ef0722bb53c8e6590c
Peter Wang <msnwangping@hotmail.com>
2022-11-21 10:18:58 +0000

    update version to 47.0.1

--- scripts/prepare_release.bat
+++ scripts/prepare_release.bat
@@ -4,11 +4,13 @@
 :: LICENSE file in the root directory of this source tree.
 
 echo off
-IF "%1"=="" GOTO:arg_not_exists
-IF "%2"=="" GOTO:arg_not_exists
+IF "%1"=="" GOTO arg_not_exists
+IF "%2"=="" GOTO arg_not_exists
 set version=%1
 set versionName=%2
 node update_API_version.js
 node update_Plugin_version.js %version% %versionName%
+goto:eof
+
 :arg_not_exists
 echo Please run this command with version and versionName. For example: prepare_release.bat 46 46.0.1
