ca53d92f2c1abe31151760d09db8f7f7f0ee7ff3
Joseph Leybovich <josephle@meta.com>
2023-05-22 14:08:57 -0400

    ssh option in install scripts

--- VoiceSDK_1click_install_scripts/install_VoiceSDK.bat
+++ VoiceSDK_1click_install_scripts/install_VoiceSDK.bat
@@ -3,11 +3,13 @@
 :: This source code is licensed under the license found in the
 :: LICENSE file in the root directory of this source tree.
 
+IF "%1"=="-ssh" (SET repoPrefix=git@github.com:wit-ai) else (SET repoPrefix=https://github.com/wit-ai)
+
 if not exist Plugins\ mkdir Plugins
 cd Plugins
 
-if not exist voicesdk-unreal\ git clone -c core.longpaths=true https://github.com/wit-ai/voicesdk-unreal
-if not exist wit-unreal\ git clone -c core.longpaths=true https://github.com/wit-ai/wit-unreal
+if not exist voicesdk-unreal\ git clone -c core.longpaths=true %repoPrefix%/voicesdk-unreal
+if not exist wit-unreal\ git clone -c core.longpaths=true %repoPrefix%/wit-unreal
 
 cd ..
 ECHO [Voice]>>.\\Config\\DefaultEngine.ini
