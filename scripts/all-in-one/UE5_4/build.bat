8b6fd0e9723d6a166ac73a13b609d494f4348132
IanSlatteryMeta <146737714+IanSlatteryMeta@users.noreply.github.com>
2024-04-23 10:58:58 -0700

    Add 5.4 build

---
+++ scripts/all-in-one/UE5_4/build.bat
@@ -0,0 +1,11 @@
+:: Copyright (c) Meta Platforms, Inc. and affiliates.
+::
+:: This source code is licensed under the license found in the
+:: LICENSE file in the root directory of this source tree.
+
+echo off
+
+SET witBranch=%1
+SET vsdkBranch=%2
+
+../utils/all_in_one_packaging.bat 5.4 %witBranch% %vsdkBranch%
